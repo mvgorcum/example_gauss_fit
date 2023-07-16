c08beb3f8074889a6774f3fed432b97e9e290e84
Mathijs van Gorcum <mathijs@vgorcum.com>
2023-07-16 23:56:44 +0200

    add docstring

--- example_gauss_fit/example_gauss_fit.py
+++ example_gauss_fit/example_gauss_fit.py
@@ -8,6 +8,28 @@ def _gaussian(x: ArrayLike, sigma: float, mu: float) -> ArrayLike:
 
 
 def gauss_fit(x: ArrayLike, y: ArrayLike) -> dict:
+    """
+    Fits a gaussian function through supplied x and y data,
+    using Levenberg-Marquardt algorithm.
+
+    Parameters
+    ----------
+    x : ArrayLike
+        x data to be fit
+    y : ArrayLike
+        y data to be fit
+
+    Returns
+    -------
+    Dictionary
+        a dictionary containing mu and sigma (the fit parameters)
+
+    Raises
+    ------
+    ValueError
+        When the length of the x array is not equal to the length of the y array
+        When the length of the x array is smaller than 2
+    """
     if len(x) != len(y):
         raise ValueError('x and y need to be of equal length')
     if len(x) < 3:
@@ -18,6 +40,21 @@ def gauss_fit(x: ArrayLike, y: ArrayLike) -> dict:
 
 
 def gauss_eval(x: list, fit: dict) -> list:
+    """
+    Returns a values from a gaussian based on sigma and mu
+    as supplied in 'fit' for the corresponding x values
+    Parameters
+    ----------
+    x : ArrayLike
+        x data to be fit
+    f : Dictionary
+        A dictionary with keys 'sigma' and 'mu' with corresponding values
+
+    Returns
+    -------
+    ArrayLike
+        The y-values of the corresponding x-values
+    """
     if 'mu' not in fit or 'sigma' not in fit:
         raise ValueError('mu and sigma are required in the fit dictionary')
     return _gaussian(x, fit['sigma'], fit['mu'])
